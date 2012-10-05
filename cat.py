9ac2fe07de3d80012cc4fb2266c5de5ee9ed0149
KitWallace <kit.wallace@gmail.com>
2012-10-05 07:59:33 +0100

    simple summing equilibrium potentials is meaningless

--- cat.py
+++ cat.py
@@ -6,7 +6,4 @@ print "Cat Motorneuron"
 cat = [("K+", +1, 150, 5.5), ("Na+", +1, 15.0, 150), ("Cl-",-1, 9, 125)] 
 ion_Vm = bio.Nernst_membrane_equilibrium(cat)  
 print "ion Vms", ion_Vm
-Vm = sum ([ion[1] for ion in ion_Vm])
-print "membrane Vm", Vm
-
 
--- frog.py
+++ frog.py
@@ -4,6 +4,4 @@ print "Frog Muscle"
 frog_muscle = [("K+", +1, 124, 2.2), ("Na+", +1, 4.0, 109.0), ("Cl-",-1, 1.5, 77.0)] # from L2
 ion_Vm = bio.Nernst_membrane_equilibrium(frog_muscle)  
 print "ion Vms", ion_Vm
-Vm = sum ([ ion[1] for ion in ion_Vm])   
-print "membrane Vm", Vm
 
